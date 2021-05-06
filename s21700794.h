88da913cb8acf762a787f8fce1e08bc345fbd2b1
21700794 <82189638+21700794@users.noreply.github.com>
2021-05-06 13:40:23 +0900

    Update s21700794.h
    Create, Update

--- s21700794.h
+++ s21700794.h
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 
-int createTodo(){
+
+typedef struct todolist{
+    int user;
+    char name[100];
+    char list[100];
+    int time;
+    int priority;
+} Todolist;
+
+int createTodo(Todolist *t){
+    printf("사용자(PIN) : ");
+    scanf(" %d", &t->user);
+    printf("사용자(username) : ");
+    scanf(" %d", t->name);
+    printf("할 일은? : ");
+    scanf(" %d", t->list);
+    printf("시간은? : ");
+    scanf(" %d", &p->time);
+    printf("우선순위는? : ");
+    scanf(" %d", &p->priority);
+    printf("=> 저장되었습니다.\n");
+    return 1; 
 }
 
-int updateTodo(){
+int updateTodo(Todolist *t){
+    printf("사용자(PIN) : ");
+    scanf(" %d", &t->user);
+    printf("사용자(username) : ");
+    scanf(" %d", t->name);
+    printf("할 일은? : ");
+    scanf(" %d", t->list);
+    printf("시간은? : ");
+    scanf(" %d", &p->time);
+    printf("우선순위는? : ");
+    scanf(" %d", &p->priority);
+    printf("=> 저장되었습니다.\n");
+    return 1; 
 }
