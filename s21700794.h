dc9ab7d795a1b6a13794c58118bdf814e797c315
Jiwon96 <jiwon96@github.com>
2021-05-08 14:54:38 +0900

    head파일이랑 c 파일이랑 분리

--- s21700794.h
+++ s21700794.h
@@ -10,45 +10,12 @@ typedef struct todolist{
     int priority;
 } Todolist;
 
-void readTodo(Todolist t){
-    printf("%4d %4s %4s %4d %4d", t.user, t.name, t.list, t.time, t.priority);
-}
+void readTodo(Todolist t);
 
-int createTodo(Todolist *t){
-    printf("사용자(PIN) : ");
-    scanf(" %d", &t->user);
-    printf("사용자(username) : ");
-    scanf(" %d", t->name);
-    printf("할 일은? : ");
-    scanf(" %d", t->list);
-    printf("시간은? : ");
-    scanf(" %d", &t->time);
-    printf("우선순위는? : ");
-    scanf(" %d", &t->priority);
-    printf("=> 저장되었습니다.\n");
-    return 1; 
-}
+void Allreadtodo(Todolist * t);
 
-int updateTodo(Todolist *t){
-    printf("사용자(PIN) : ");
-    scanf(" %d", &t->user);
-    printf("사용자(username) : ");
-    scanf(" %d", t->name);
-    printf("할 일은? : ");
-    scanf(" %d", t->list);
-    printf("시간은? : ");
-    scanf(" %d", &t->time);
-    printf("우선순위는? : ");
-    scanf(" %d", &t->priority);
-    printf("=> 저장되었습니다.\n");
-    return 1; 
-}
+int createTodo(Todolist *t);
 
-int deleteTodo(Todolist *t){
-    t->user = -1;
-//    t->name = -1;
-//    t->list = -1;
-    t->time = -1;
-    t->priority = -1;
-    return 1;
-}
+int updateTodo(Todolist *t);
+
+int deleteTodo(Todolist *t);
