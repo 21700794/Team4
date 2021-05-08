bac31e9eb4ba6e55a534af19a2207471e912e885
21700794 <82189638+21700794@users.noreply.github.com>
2021-05-09 00:43:15 +0900

    5/9 12:43 수정

--- s21700794.c
+++ s21700794.c
@@ -17,6 +17,7 @@ void Allreadtodo(Todolist *t)
     for(int i =0; i<SIZE; i++)
     {
         if(t[i].user == -1) continue;
+        printf("%d ", i+1);
         readTodo(t[i]);
     }
 }
@@ -26,9 +27,9 @@ int createTodo(Todolist *t){
     printf("사용자(PIN) : ");
     scanf(" %d", &t->user);
     printf("사용자(username) : ");
-    scanf(" %s", t->name);
+    scanf(" %[^\n]s", t->name);
     printf("할 일은? : ");
-    scanf(" %s", t->list);
+    scanf(" %[^\n]s", t->list);
     printf("시간은? : ");
     scanf(" %d", &t->time);
     printf("우선순위는? : ");
@@ -41,9 +42,9 @@ int updateTodo(Todolist *t){
     printf("사용자(PIN) : ");
     scanf(" %d", &t->user);
     printf("사용자(username) : ");
-    scanf(" %s", t->name);
+    scanf(" %[^\n]s", t->name);
     printf("할 일은? : ");
-    scanf(" %s", t->list);
+    scanf(" %[^\n]s", t->list);
     printf("시간은? : ");
     scanf(" %d", &t->time);
     printf("우선순위는? : ");
@@ -68,4 +69,22 @@ int selectDataNo(Todolist *t, int index)
 	printf("\n번호는 (취소: 0)?");
 	scanf(" %d", &no);
 	return no;
+}
+
+int menu(){
+        int num;
+        printf("\n*** Todolist ***\n");
+        printf("1. 조회\n");
+        printf("2. 추가\n");
+        printf("3. 수정\n");
+        printf("4. 삭제\n");
+        //printf("5. 파일 저장\n");
+        //printf("6. 이름으로 검색\n");
+        //printf("7. 별점으로 검색\n");
+        //printf("8. 가격으로 검색\n");
+        printf("0. 종료\n\n");
+        printf("=> 원하는 메뉴는? ");
+        scanf("%d", &num);
+
+        return num;
 }
